9bcbb9d79116af14a4eaedaafbf8b2fce852fb74
Samaelish <semnd007@gmail.com>
2024-08-18 15:12:44 +0300

    created a root layout component with a theme provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,38 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+/**
+ * Root layout component for the application.
+ * This component wraps the entire application and provides a basic structure for the HTML document.
+ * It also sets up the theme provider for the application.
+ */
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { ThemeProvider } from 'next-themes'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
+
+/**
+ * Metadata for the application.
+ *
+ * This object contains metadata about the application, such as the title and description.
+ */
 
 export const metadata: Metadata = {
   title: "Semyon's Portfolio",
-  description: "Современное и минималистичное портфолио",
-};
+  description: 'Современное и минималистичное портфолио',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
